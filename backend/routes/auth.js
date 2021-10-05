6fd953dd2fd00808093896412b7f5875ef0619f4
Budhathoki-Nirmal <nirmbudhathoki@gmail.com>
2021-10-05 08:34:39 +0545

    Using JSON Web Token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,6 +4,8 @@ const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "AuthenticationToken";
 // const { findOne } = require('../models/User');
 
 
@@ -25,17 +27,23 @@ router.post('/createuser', [
     if (user) {
       return res.status(400).json({ errors: 'Sorry User already exists' });
     }
-    
     const salt = await bcrypt.genSalt(10);
-    const secPass= await bcrypt.hash(req.body.password, salt);
+    const secPass = await bcrypt.hash(req.body.password, salt);
+
     //create a new user
     user = await User.create({
       name: req.body.name,
       email: req.body.email,
       password: secPass,
-    })
-    //   .then(user => res.json(user)).catch(err=> {console.log(err)
-    res.json({ user })
+    });
+
+    const data = {
+      user: {
+        id: user.id
+      }
+    }
+    const authtoken = jwt.sign(data, JWT_SECRET);
+    res.json({ authtoken });
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Some errror occured");
