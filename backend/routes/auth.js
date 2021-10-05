043aef50c1c51cf1089f35d9f932ae0c277946a8
Budhathoki-Nirmal <nirmbudhathoki@gmail.com>
2021-10-05 10:02:00 +0545

    login endpoint and sending auth token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+var jwt = require('jsonwebtoken');
 const JWT_SECRET = "AuthenticationToken";
 // const { findOne } = require('../models/User');
 
@@ -48,6 +48,39 @@ router.post('/createuser', [
     console.error(error.message);
     res.status(500).send("Some errror occured");
 
+  }
+})
+
+//Authenticate a user using: POST "/api/auth/login"
+router.post('/login', [
+  body('email', 'Enter Valid Email').isEmail(),
+  body('password', 'Password never blank').exists(),
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  //retrieve email and password from req.body
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({email});
+    if (!user) {
+      return res.status(400).json({ errors: 'Please enter valid credentials' });
+    }
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+      return res.status(400).json({ errors: 'Please enter valid credentials' });
+    }
+    const data = {
+      user: {
+        id: user.id
+      }
+    }
+    const authtoken = jwt.sign(data, JWT_SECRET);
+    res.json({ authtoken })
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal errror occured");
   }
 })
 module.exports = router
