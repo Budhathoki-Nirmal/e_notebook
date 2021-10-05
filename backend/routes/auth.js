129b13e22f3fa27fdee307dd3036d5ecb40ce6b9
Budhathoki-Nirmal <nirmbudhathoki@gmail.com>
2021-10-05 08:01:11 +0545

    Password hashing using bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,7 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 // const { findOne } = require('../models/User');
 
 
@@ -24,11 +25,14 @@ router.post('/createuser', [
     if (user) {
       return res.status(400).json({ errors: 'Sorry User already exists' });
     }
+    
+    const salt = await bcrypt.genSalt(10);
+    const secPass= await bcrypt.hash(req.body.password, salt);
     //create a new user
     user = await User.create({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
+      password: secPass,
     })
     //   .then(user => res.json(user)).catch(err=> {console.log(err)
     res.json({ user })
